79abf8026668e5deb353a3140e1f3a19acf707e2
Luuk Altenburg <58938133+luukalt@users.noreply.github.com>
2023-11-15 23:25:38 +0100

    u

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function handleCredentialResponse(response) {
     console.log('Last Name:', userDetails.lastName);
     console.log('Email:', userDetails.email);
 
-    document.getElementById('g_id_signin').innerText = userDetails.firstName + " " + userDetails.lastName;
+    // Hide the Google sign-in button
 
     // Optionally update these details in your HTML
     // Ensure your HTML has elements with these IDs
@@ -43,41 +43,12 @@ function extractUserDetails(token) {
 }
 
 
-
-// function handleCredentialResponse(response) {
-//     // Handle the ID token in response
-//     console.log("Encoded JWT ID token: " + response.credential);
-//     }
-
-// function decodeJwtToken(token) {
-//     const base64Url = token.split('.')[1]; // Get the payload part
-//     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-//     const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-//         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-//     }).join(''));
-    
-//     return JSON.parse(jsonPayload);
-//     }
-    
-//     // Function to extract first name, last name, and email
-//     function extractUserDetails(token) {
-//     const userData = decodeJwtToken(token);
-//     const fullName = userData.name || ''; // Full name
-//     const email = userData.email || '';
-    
-//     // Splitting the full name into first and last names
-//     // This is a basic split and might not work correctly for all name formats
-//     const names = fullName.split(' ');
-//     const firstName = names[0] || '';
-//     const lastName = names.length > 1 ? names[names.length - 1] : '';
-    
-//     return { firstName, lastName, email };
-//     }
-  
 window.onload = function () {
     google.accounts.id.initialize({
         client_id: "1050074680030-80b20cr4ud2h6kt59pdkbtc63bvhus03.apps.googleusercontent.com",
-        callback: handleCredentialResponse
+        callback: handleCredentialResponse,
+        prompt_parent_id: 'g_id_signin',
+        auto_select: false, // This ensures the account chooser is always shown
     });
 
     google.accounts.id.renderButton(
@@ -85,5 +56,5 @@ window.onload = function () {
         { theme: "outline", size: "large", text: "sign_in_with" } // Customization attributes
     );
 
-    google.accounts.id.prompt(); // Display the One Tap prompt
+    // google.accounts.id.prompt(); // Display the One Tap prompt
     }
