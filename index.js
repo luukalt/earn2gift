a18b33d981060f7e5a3c35937fb2063e92ae4c9f
Luuk Altenburg <58938133+luukalt@users.noreply.github.com>
2023-11-16 00:41:11 +0100

    u

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+// Function to handle the response from Google sign-in
 function handleCredentialResponse(response) {
     console.log("Encoded JWT ID token: " + response.credential);
 
-    // Extract user details from the ID token
+    // Extract user details from the ID token (implement extractUserDetails)
     const userDetails = extractUserDetails(response.credential);
+
+    // Update UI with user details
+    document.getElementById('user_name').innerText = userDetails.firstName + ' ' + userDetails.lastName;
+    document.getElementById('user_email').innerText = userDetails.email;
+    document.getElementById('user_info').style.display = 'block';
+
+    // Hide the Google sign-in button
+    document.getElementById('g_id_signin').style.display = 'none';
 
     // Log or use the user details
     console.log('First Name:', userDetails.firstName);
     console.log('Last Name:', userDetails.lastName);
     console.log('Email:', userDetails.email);
 }
+
 
 function decodeJwtToken(token) {
     const base64Url = token.split('.')[1]; // Get the payload part
@@ -35,17 +45,42 @@ function extractUserDetails(token) {
 }
 
 
+// Function to reinitialize the Google Sign-In button
+function reinitializeGoogleSignIn() {
+    // Clear existing content
+    const signInDiv = document.getElementById('g_id_signin');
+    signInDiv.innerHTML = '';
+
+    // Rerender the Google Sign-In Button
+    google.accounts.id.renderButton(
+        signInDiv,
+        { theme: "outline", size: "large", text: "sign_in_with" }
+    );
+
+    // Show the Google sign-in button
+    signInDiv.style.display = 'block';
+}
+
+// Function to sign out the user
+function signOutUser() {
+    // Code to handle user sign-out (e.g., revoke token, clear session)
+
+    // Hide user info and reinitialize Google Sign-In
+    document.getElementById('user_info').style.display = 'none';
+    reinitializeGoogleSignIn();
+}
+
 window.onload = function () {
     google.accounts.id.initialize({
-        client_id: "1050074680030-80b20cr4ud2h6kt59pdkbtc63bvhus03.apps.googleusercontent.com",
+        client_id: "80b20cr4ud2h6kt59pdkbtc63bvhus03.apps.googleusercontent.com",
         callback: handleCredentialResponse,
-        auto_select: false, // This ensures the account chooser is always shown
+        auto_select: false
     });
 
     google.accounts.id.renderButton(
         document.getElementById("g_id_signin"),
-        { theme: "outline", size: "large", text: "sign_in_with" } // Customization attributes
+        { theme: "outline", size: "large", text: "sign_in_with" }
     );
 
-    // google.accounts.id.prompt(); // Display the One Tap prompt
-    }
+    // Optionally call google.accounts.id.prompt(); if needed
+};
